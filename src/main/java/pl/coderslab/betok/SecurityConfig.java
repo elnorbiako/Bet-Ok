33d75e427b46f03b92b9976e0c3e4b225366b5f8
Norbert Różyc <elnorbiako@gmail.com>
2018-06-05 09:52:55 +0200

    added Swagger integration

--- src/main/java/pl/coderslab/betok/SecurityConfig.java
+++ src/main/java/pl/coderslab/betok/SecurityConfig.java
@@ -15,7 +15,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 .antMatchers("/login", "/", "/register").anonymous()
-                .antMatchers("/admin/**").hasRole("ADMIN")
+                .antMatchers("/admin/**", "/swagger-ui.html").hasRole("ADMIN")
                 .antMatchers("/*").authenticated()
             .and()
                 .formLogin()
